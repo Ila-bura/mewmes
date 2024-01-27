ee56335b77607fea10db304c99f905fa8f98f3e5
Ila-bura <>
2024-01-27 08:48:50 +0000

    Create styles folder and style navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,11 +2,12 @@ import React from 'react';
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
+import styles from "../styles/NavBar.module.css";
 import logo from '../assets/logo.png'
 
 const NavBar = () => {
   return (
-    <Navbar expand="md" fixed="top">
+    <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
         <Navbar.Brand>
           <img src={logo} alt="logo" height="125" />
