f7df73e0ad7e3ce2df14fa13391b9cac9aca3250
Ila-bura <>
2024-01-27 09:56:41 +0000

    Add routing to navbar links and style to active links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,34 +3,37 @@ import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import styles from "../styles/NavBar.module.css";
-import logo from '../assets/logo.png'
+import logo from '../assets/logo.png';
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
+      <NavLink to="/">
         <Navbar.Brand>
           <img src={logo} alt="logo" height="125" />
         </Navbar.Brand>
+        </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
-            <Nav.Link>
-            <i class="fas fa-house-user"></i>
+            <NavLink exact className={styles.NavLink} activeClassName={styles.Active} to="/">
+            <i className="fas fa-house-user"></i>
             Home
-            </Nav.Link>
-            <Nav.Link>
-            <i class="far fa-question-circle"></i>
+            </NavLink>
+            <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/about">
+            <i className="far fa-question-circle"></i>
             About
-            </Nav.Link>
-            <Nav.Link>
-            <i class="far fa-arrow-alt-circle-right"></i>
+            </NavLink>
+            <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/signin">
+            <i className="far fa-arrow-alt-circle-right"></i>
             Sign In
-            </Nav.Link>
-            <Nav.Link>
-            <i class="fas fa-plus-circle"></i>
+            </NavLink>
+            <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/signup">
+            <i className="fas fa-plus-circle"></i>
             Sign Up
-            </Nav.Link>
+            </NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
