df6880733845688b37c2ea672147023d7998881d
Ila-bura <>
2024-01-29 16:44:26 +0000

    Create basic structure for PostCreateForm

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Route, Switch } from "react-router-dom";
 import "./api/axiosDefaults";
 import SignUpForm from './pages/auth/SignUpForm';
 import SignInForm from './pages/auth/SignInForm';
+import PostCreateForm from './pages/posts/PostCreateForm';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
             <Switch>
             <Route exact path="/" render={() => <h1>Home page</h1>} />
             <Route exact path="/about" render={() => <h1>About</h1>} />
+            <Route exact path='/posts/create' render={() => <PostCreateForm />} />
             <Route exact path='/signin' render={() => <SignInForm />} />
             <Route exact path="/signup" render={() => <SignUpForm />} />
             <Route render={() => <p>Page not found!</p>} />
