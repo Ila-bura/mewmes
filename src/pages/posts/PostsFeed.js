24bfce8a8647d5c1c47985ca399eb1555b2afd69
Ila-bura <>
2024-02-01 19:53:58 +0000

    Edit loader spinner apperance

--- src/pages/posts/PostsFeed.js
+++ src/pages/posts/PostsFeed.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { BeatLoader } from "react-spinners"; 
 
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
@@ -53,8 +54,9 @@ function PostsFeed({ message, filter = "" }) {
             )}
           </>
         ) : (
+            // Render spinner while loading
           <Container className={appStyles.Content}>
-            <Asset spinner />
+            <BeatLoader color="#36d7b7" />
           </Container>
         )}
       </Col>
