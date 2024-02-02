5b9227bc11e34e9d417bdceeacc56b7b04ea95ab
Ila-bura <>
2024-02-02 23:48:31 +0000

    StyleFix spacing and font colors of owner and date stamp

--- src/pages/replies/Reply.js
+++ src/pages/replies/Reply.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Media } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Avatar from "../../components/Avatar";
-import styles from "../../styles/ReplyForm.module.css";
+import styles from "../../styles/Reply.module.css";
 
 const Reply = (props) => {
   const { profile_id, profile_image, owner, updated_at, content } = props;
