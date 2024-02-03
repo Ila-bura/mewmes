d74907535e3b1fdf8b5771ca5258cddad2ecad9b
Ila-bura <>
2024-02-03 13:11:40 +0000

    Add profile avatar in user profile component

--- src/pages/profiles/ProfilePage.js
+++ src/pages/profiles/ProfilePage.js
@@ -8,24 +8,53 @@ import Asset from "../../components/Asset";
 
 import styles from "../../styles/ProfilePage.module.css";
 import appStyles from "../../App.module.css";
-import btnStyles from "../../styles/Button.module.css";
+import { Image } from "react-bootstrap";
 
 import PopularProfiles from "./PopularProfiles";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import { useParams } from "react-router";
+import { axiosReq } from "../../api/axiosDefaults";
+import {
+    useProfileData,
+    useSetProfileData,
+} from "../../contexts/ProfileDataContext";
 
 function ProfilePage() {
   const [hasLoaded, setHasLoaded] = useState(false);
+  const [profilePosts, setProfilePosts] = useState({ results: [] });
+  
   const currentUser = useCurrentUser();
+  const { id } = useParams();
+  const { setProfileData} = useSetProfileData();
+  const { pageProfile } = useProfileData();
+  const [profile] = pageProfile.results;
 
   useEffect(() => {
-      setHasLoaded(true);
-  }, [])
+    const fetchData = async () => {
+        try {
+            const [{ data: pageProfile }, { data: profilePosts }] =
+                await Promise.all([
+                    axiosReq.get(`/profiles/${id}/`),
+                    axiosReq.get(`/posts/?owner__profile=${id}`),
+                ]);
+            setProfileData((prevState) => ({
+                ...prevState,
+                pageProfile: { results: [pageProfile] },
+            }));
+            setProfilePosts(profilePosts);
+            setHasLoaded(true);
+        } catch (err) {
+            // console.log(err);
+        }
+    };
+    fetchData();
+}, [id, setProfileData]);
 
   const mainProfile = (
     <>
       <Row noGutters className="px-3 text-center">
         <Col lg={3} className="text-lg-left">
-          <p>Image</p>
+        <Image className={styles.ProfileImage} roundedCircle src={profile?.image} alt="avatar"/>
         </Col>
         <Col lg={6}>
           <h3 className="m-2">Profile username</h3>
