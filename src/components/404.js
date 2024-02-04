125ee3f3b4587aebcff2212ff7b6c6ea7cdbd4d7
Ila-bura <>
2024-02-04 20:31:41 +0000

    Style 404 page

--- src/components/404.js
+++ src/components/404.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import NotFound from "../assets/notfound.png";
 import Asset from './Asset';
-import styles from "../styles/404.module.css";
+import styles from "../styles/NotFound.module.css";
+import { useHistory } from 'react-router-dom';
+import { BeatLoader } from "react-spinners"; 
 
 
 const NotFound404 = () => {
+    const history = useHistory();
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            history.push('/');
+        }, 5000);
+
+        return () => clearTimeout(timeout);
+    }, [history]);
+
     return (
         <div className={styles.NotFound}>
+            <div className={styles.LoaderContainer}>
+                <BeatLoader color="#36D7B7" />
+            </div>
             <Asset
                 src={NotFound}
-                message={`Oh oh! Nothing to see here, sorry!`}
+                message={`This page does not exist. You'll be redirected to the HomePage now!`}
             />
         </div>
     );
