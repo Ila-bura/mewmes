80b33bd337a777fc60f48311c1bbb6803f0e43bf
Ila-bura <>
2024-02-04 20:07:10 +0000

    Add OverlayTrigger to MoreDropdown menu

--- src/components/MoreDropdown.js
+++ src/components/MoreDropdown.js
@@ -2,39 +2,56 @@ import React from "react";
 import Dropdown from "react-bootstrap/Dropdown";
 import styles from "../styles/MoreDropdown.module.css";
 import { useHistory } from "react-router";
-
+import { OverlayTrigger, Tooltip } from "react-bootstrap";
 
 const ThreeDrops = React.forwardRef(({ onClick }, ref) => (
-    <i
-      className="fas fa-ellipsis-v"
-      ref={ref}
-      onClick={(e) => {
-        e.preventDefault();
-        onClick(e);
-      }}
-    />
-  ));
+  <i
+    className="fas fa-ellipsis-v"
+    ref={ref}
+    onClick={(e) => {
+      e.preventDefault();
+      onClick(e);
+    }}
+  />
+));
 
 const ProfileSettings = React.forwardRef(({ onClick }, ref) => (
-    <i
-      className="fas fa-bars"
-      ref={ref}
-      onClick={(e) => {
-        e.preventDefault();
-        onClick(e);
-      }}
-    />
-  ));
-  
-  // Dropdown component for edit and delete options
-  export const MoreDropdown = ({ handleEdit, handleDelete }) => {
-    return (
-        <Dropdown className={`ml-auto" ${styles.Absolute}`} drop="right">
-        <Dropdown.Toggle as={ThreeDrops} />
-  
-        <Dropdown.Menu
-          className="text-center"
-          popperConfig={{ strategy: "fixed" }}
+  <i
+    className="fas fa-bars"
+    ref={ref}
+    onClick={(e) => {
+      e.preventDefault();
+      onClick(e);
+    }}
+  />
+));
+
+const renderTooltip1 = (props) => (
+  <Tooltip id="button-tooltip" {...props}>
+    Edit meme
+  </Tooltip>
+);
+
+const renderTooltip2 = (props) => (
+  <Tooltip id="button-tooltip" {...props}>
+    Delete meme
+  </Tooltip>
+);
+
+// Dropdown component for edit and delete options
+export const MoreDropdown = ({ handleEdit, handleDelete }) => {
+  return (
+    <Dropdown className={`ml-auto" ${styles.Absolute}`} drop="left">
+      <Dropdown.Toggle as={ThreeDrops} />
+
+      <Dropdown.Menu
+        className="text-center"
+        popperConfig={{ strategy: "fixed" }}
+      >
+        <OverlayTrigger
+          placement="top"
+          delay={{ show: 250, hide: 400 }}
+          overlay={renderTooltip1}
         >
           <Dropdown.Item
             className={styles.DropdownItem}
@@ -43,6 +60,12 @@ const ProfileSettings = React.forwardRef(({ onClick }, ref) => (
           >
             <i className="far fa-edit" />
           </Dropdown.Item>
+        </OverlayTrigger>
+        <OverlayTrigger
+          placement="top"
+          delay={{ show: 250, hide: 400 }}
+          overlay={renderTooltip2}
+        >
           <Dropdown.Item
             className={styles.DropdownItem}
             onClick={handleDelete}
@@ -50,41 +73,40 @@ const ProfileSettings = React.forwardRef(({ onClick }, ref) => (
           >
             <i className="far fa-trash-alt" />
           </Dropdown.Item>
-        </Dropdown.Menu>
-      </Dropdown>
-    );
-  };
+        </OverlayTrigger>
+      </Dropdown.Menu>
+    </Dropdown>
+  );
+};
 
-  // Dropdown component for profile edit options
-  export function ProfileEditDropdown({ id }) {
-    const history = useHistory();
-    return (
-      <Dropdown className={`ml-auto px-3 ${styles.Bars}`} drop="left">
-        <Dropdown.Toggle as={ProfileSettings} />
-        <Dropdown.Menu>
-  
-          <Dropdown.Item
-            onClick={() => history.push(`/profiles/${id}/edit`)}
-            aria-label="edit-profile"
-          >
-            <i className="fas fa-user-pen" /> Edit profile
-          </Dropdown.Item>
-          <Dropdown.Item
-            onClick={() => history.push(`/profiles/${id}/edit/username`)
-            }
-            aria-label="edit-username"
-          >
-            <i className="fas fa-people-arrows" />
-            Change username
-          </Dropdown.Item>
-          <Dropdown.Item
-            onClick={() => history.push(`/profiles/${id}/edit/password`)}
-            aria-label="edit-password"
-          >
-            <i className="fas fa-key" />
-            Change password
-          </Dropdown.Item>
-        </Dropdown.Menu>
-      </Dropdown>
-    );
-  }
+// Dropdown component for profile edit options
+export function ProfileEditDropdown({ id }) {
+  const history = useHistory();
+  return (
+    <Dropdown className={`ml-auto px-3 ${styles.Bars}`} drop="left">
+      <Dropdown.Toggle as={ProfileSettings} />
+      <Dropdown.Menu>
+        <Dropdown.Item
+          onClick={() => history.push(`/profiles/${id}/edit`)}
+          aria-label="edit-profile"
+        >
+          <i className="fas fa-user-pen" /> Edit profile
+        </Dropdown.Item>
+        <Dropdown.Item
+          onClick={() => history.push(`/profiles/${id}/edit/username`)}
+          aria-label="edit-username"
+        >
+          <i className="fas fa-people-arrows" />
+          Change username
+        </Dropdown.Item>
+        <Dropdown.Item
+          onClick={() => history.push(`/profiles/${id}/edit/password`)}
+          aria-label="edit-password"
+        >
+          <i className="fas fa-key" />
+          Change password
+        </Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+  );
+}
